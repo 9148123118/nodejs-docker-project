6afac6b3dc658016e197c663160907b497349cae
9148123118 <114432978+9148123118@users.noreply.github.com>
2022-10-18 10:34:56 +0530

    Yeshwanth added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Jani, Welcome to Artivatic');
+  res.send('Hello Yeshwanth, Welcome to Artivatic');
 });
 
 app.listen(PORT, HOST);
