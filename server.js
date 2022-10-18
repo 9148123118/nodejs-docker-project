72aab46ec5ac8888b47fde00d39fc8004a43dd72
9148123118 <114432978+9148123118@users.noreply.github.com>
2022-10-18 11:22:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Ramachandra, Welcome to Artivatic');
+  res.send('Hello Theja, Welcome to Artivatic');
 });
 
 app.listen(PORT, HOST);
